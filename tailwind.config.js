8b1a9118c8b0c2eb22379bcf2fb84d8bb9af96fe
natsu <nyny.sugar@gmail.com>
2022-07-12 19:46:37 +0900

    theme-screens,colors,font設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,45 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      "2xl": "1920px",
+    },
+    colors: {
+      base: "#E5D5C6",
+      main: "#C97D68",
+      accent: "#C6CDBC",
+    },
+    fontFamily: {
+      body: ["YuGothic", "Yu Gothic", "sans-serif"],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "90%",
+          "@screen sm": {
+            maxWidth: "360px",
+          },
+          "@screen md": {
+            maxWidth: "700px",
+          },
+          "@screen lg": {
+            maxWidth: "900px",
+          },
+          "@screen xl": {
+            maxWidth: "1200px",
+          },
+          "@screen 2xl": {
+            maxWidth: "1200px",
+          },
+        },
+      });
+    },
+  ],
 };
