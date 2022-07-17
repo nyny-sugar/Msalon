69a2c94664cafc067b2216ad1248d028cf1b20cd
natsu <nyny.sugar@gmail.com>
2022-07-17 17:55:31 +0900

    animation:upin作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,9 +28,14 @@ module.exports = {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
         },
+        upin: {
+          "0%": { transform: "translateY(100%)" },
+          "100%": { transform: "translateY(0)" },
+        },
       },
       animation: {
         fadein: "fadein 1.5s ease-in-out",
+        upin: "upin 2s ease",
       },
     },
     plugins: [
