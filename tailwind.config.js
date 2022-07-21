6c58d1dd0171f974a0138b79f2cfe820e796c2ab
natsu <nyny.sugar@gmail.com>
2022-07-21 20:19:33 +0900

    top画:animation追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,45 +26,45 @@ module.exports = {
       "16/9": "1920/1080",
       "4/3": "4/3",
     },
-  },
-  extend: {
-    keyframes: {
-      fadein: {
-        "0%": { opacity: 0 },
-        "100%": { opacity: 1 },
-      },
-      upin: {
-        "0%": { opacity: 0, transform: "translateY(100%)" },
-        "100%": { opacity: 1, transform: "translateY(0)" },
-      },
-    },
-    animation: {
-      fadein: "fadein 1.5s ease-in-out",
-      upin: "upin 6s ease",
-    },
-  },
-  plugins: [
-    function ({ addComponents }) {
-      addComponents({
-        ".container": {
-          maxWidth: "90%",
-          "@screen sm": {
-            maxWidth: "360px",
-          },
-          "@screen md": {
-            maxWidth: "700px",
-          },
-          "@screen lg": {
-            maxWidth: "900px",
-          },
-          "@screen xl": {
-            maxWidth: "1200px",
-          },
-          "@screen 2xl": {
-            maxWidth: "1200px",
-          },
+    extend: {
+      keyframes: {
+        fadein: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
-      });
+        upin: {
+          "0%": { opacity: 0, transform: "translateY(100%)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadein: "fadein 1.5s ease-in-out",
+        upin: "upin 6s ease",
+      },
     },
-  ],
+    plugins: [
+      function ({ addComponents }) {
+        addComponents({
+          ".container": {
+            maxWidth: "90%",
+            "@screen sm": {
+              maxWidth: "360px",
+            },
+            "@screen md": {
+              maxWidth: "700px",
+            },
+            "@screen lg": {
+              maxWidth: "900px",
+            },
+            "@screen xl": {
+              maxWidth: "1200px",
+            },
+            "@screen 2xl": {
+              maxWidth: "1200px",
+            },
+          },
+        });
+      },
+    ],
+  },
 };
