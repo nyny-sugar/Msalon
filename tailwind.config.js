36cfaf956a940687ddf7f7b13d72c54a1f295e94
natsu <nyny.sugar@gmail.com>
2022-07-22 21:04:10 +0900

    plugins際設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,29 +39,29 @@ module.exports = {
         fadein: "fadein 1.5s ease-in-out",
       },
     },
-    plugins: [
-      function ({ addComponents }) {
-        addComponents({
-          ".container": {
-            maxWidth: "90%",
-            "@screen sm": {
-              maxWidth: "360px",
-            },
-            "@screen md": {
-              maxWidth: "700px",
-            },
-            "@screen lg": {
-              maxWidth: "900px",
-            },
-            "@screen xl": {
-              maxWidth: "1200px",
-            },
-            "@screen 2xl": {
-              maxWidth: "1200px",
-            },
-          },
-        });
-      },
-    ],
   },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "90%",
+          "@screen sm": {
+            maxWidth: "360px",
+          },
+          "@screen md": {
+            maxWidth: "700px",
+          },
+          "@screen lg": {
+            maxWidth: "900px",
+          },
+          "@screen xl": {
+            maxWidth: "1200px",
+          },
+          "@screen 2xl": {
+            maxWidth: "1200px",
+          },
+        },
+      });
+    },
+  ],
 };
